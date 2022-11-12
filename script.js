79c1945e9d05c2a97b5bdf65fbf1a22aa068435a
O Butt <60117081+o-butt@users.noreply.github.com>
2022-11-12 21:13:46 +0000

    Update index to force CF cache invalidation and script to include new api endpoint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('wow, such visitors.');
 
 
-fetch('https://tomx98y9ib.execute-api.us-east-1.amazonaws.com/prod')
+fetch('https://ga82v3zdxf.execute-api.us-east-1.amazonaws.com/prod/GET')
     .then(response => response.json())
     .then((data) => {
         document.getElementById('idgoeshere').innerText = data.count
