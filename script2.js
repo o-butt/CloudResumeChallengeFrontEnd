aacc356a54144d980f0dd50add5602e37c3c0464
O Butt <60117081+o-butt@users.noreply.github.com>
2022-11-14 20:53:00 +0000

    Re-design

---
+++ script2.js
@@ -0,0 +1,8 @@
+console.log('wow, such visitors.');
+
+
+fetch('https://ga82v3zdxf.execute-api.us-east-1.amazonaws.com/prod/GET')
+    .then(response => response.json())
+    .then((data) => {
+        document.getElementById('idgoeshere').innerText = data.count
+    })
